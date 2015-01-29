08e0c7638edb120f72efb118d1ce6aae13f8e89a
Rayme Silverberg <rsilverberg@fullsail.edu>
2015-01-28 21:12:10 -0600

    commit 3

--- Silverberg_Rayme_Assignment4.js
+++ Silverberg_Rayme_Assignment4.js
@@ -21,3 +21,22 @@ if (length === 12 && characterAt3 === "-" && characterAt7 === "-" && isNaN(first
 }
 
 }
+
+// Title-case a string (split into words, then uppercase the first letter of each word).
+function titleCase(args) {
+    var originalString = args;
+    var splitUp = originalString.split(" ");
+    var newWords = [];
+    for (i=0; i < splitUp.length - 1; i++) {
+        var lowercase = splitUp[i].toLowerCase();
+        var capitalize = lowercase.charAt(0).toUpperCase() + lowercase.slice(1);
+        newWords.push(capitalize);
+    }
+    return newWords.join(" ");
+}
+
+//Given a string version of a number, such as Ò42Ó, return the value as an actual Number data type, such as 42.
+function returnNumber(args) {
+    var stringNumber = args;
+    return parseInt(stringNumber); 
+}   
